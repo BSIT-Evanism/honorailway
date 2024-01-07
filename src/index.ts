e9602216ab3c7bdaa1852abc436a75fb3c194f51
BSIT-Evanism <johnevanporcioncula.solanoy@bicol-u.edu.ph>
2024-01-07 15:44:49 +0800

    added test route

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,22 @@
 import { Hono } from 'hono'
+import { cors } from 'hono/cors'
 
 const app = new Hono()
 
+app.use(cors())
+
 app.get('/', (c) => {
   return c.text('Hello Hono!')
+})
+
+app.get('/json/:id', (c) => {
+  const id = c.req.param('id');
+  const page = c.req.query('page');
+
+  return c.json({
+    idName: id,
+    pageNumber: page
+  })
 })
 
 export default app
